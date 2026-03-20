e717c2e20f4a84edf48946f986d931d3d9e086e0
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-03-20 13:40:15 -0700

    format using air

--- inst/Shiny/server.r
+++ inst/Shiny/server.r
@@ -5,28 +5,31 @@ server <- function(input, output, session) {
   require(ggplot2)
   require(shiny)
 
- tmp <- tempfile()
+  tmp <- tempfile()
   onSessionEnded(function() {
-    unlink(tmp) })
+    unlink(tmp)
+  })
 
   rd_database <- reactive({
     tools::Rd_db(package = "fishdictionary")
   })
 
   output$choose_topic <- renderUI({
-    selectInput("Term", "Select term or function",
-    sub(".Rd", "", names(rd_database())))
+    selectInput(
+      "Term",
+      "Select term or function",
+      sub(".Rd", "", names(rd_database()))
+    )
   })
 
   output$documentation <- renderUI({
     rdfile <- paste0(input$Term, ".Rd")
     req(rdfile %in% names(rd_database()))
-    tools::Rd2HTML(rd_database()[[rdfile]], tmp,
-    package = "fishdictionary")
-    htmltools::includeHTML(tmp)})
+    tools::Rd2HTML(rd_database()[[rdfile]], tmp, package = "fishdictionary")
+    htmltools::includeHTML(tmp)
+  })
 
   output$foodweb <- renderPlot({
-    mvbutils::foodweb(where = "package:fishdictionary",
-     prune = input$Term)
+    mvbutils::foodweb(where = "package:fishdictionary", prune = input$Term)
   })
 }
