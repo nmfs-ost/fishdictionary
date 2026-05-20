f48e4e7742d71ce9944fe2a86a1d3a4823ae9998
Chantel Wetzel - NOAA Federal <chantel.wetzel@noaa.gov>
2026-05-20 06:48:31 -0700

    fix package name

--- R/add_object.R
+++ R/add_object.R
@@ -5,12 +5,24 @@
 #' @return An R list in the JSON format which includes the new term.
 #' @export
 add_object <- function(input_list) {
-  json_obj <- jsonlite::fromJSON(system.file("extdata","top20.json", package = "stockassessmentdictionary"))
+  json_obj <- jsonlite::fromJSON(system.file(
+    "extdata",
+    "top20.json",
+    package = "fishdictionary"
+  ))
   # Return an error if the object already exists
-  if (input_list$name %in% sapply(json_obj, get, x = "name")) stop(input_list$name, " already exists.")
-  # Validate the json input and return an error if it fails 
-  if (jsonvalidate::json_validate(json = jsonlite::toJSON(input_list, auto_unbox = TRUE),
-   schema = file.path("inst", "extdata", "schema.json"), verbose = TRUE, error = TRUE)) {
+  if (input_list$name %in% sapply(json_obj, get, x = "name")) {
+    stop(input_list$name, " already exists.")
+  }
+  # Validate the json input and return an error if it fails
+  if (
+    jsonvalidate::json_validate(
+      json = jsonlite::toJSON(input_list, auto_unbox = TRUE),
+      schema = file.path("inst", "extdata", "schema.json"),
+      verbose = TRUE,
+      error = TRUE
+    )
+  ) {
     list_length <- length(json_obj)
     json_obj[[list_length + 1]] <- input_list
     return(json_obj)
